61b4340dd7d3a86948b7a4a990c8640e6cdbacec
Richard Hutchings R <richard.hutchings@ericsson.com>
2018-08-08 14:22:39 +0100

    Adding boilerplate
    Added the boilerplate, created from http://wppb.me/

---
+++ includes/class-iamrssp-activator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       http://www.bistantum.co.uk
+ * @since      1.0.0
+ *
+ * @package    Iamrssp
+ * @subpackage Iamrssp/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    Iamrssp
+ * @subpackage Iamrssp/includes
+ * @author     Richard Hutchings <richard@bistantum.co.uk>
+ */
+class Iamrssp_Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+
+	}
+
+}
---
+++ includes/class-iamrssp-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       http://www.bistantum.co.uk
+ * @since      1.0.0
+ *
+ * @package    Iamrssp
+ * @subpackage Iamrssp/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Iamrssp
+ * @subpackage Iamrssp/includes
+ * @author     Richard Hutchings <richard@bistantum.co.uk>
+ */
+class Iamrssp_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
